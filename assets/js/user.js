f5061749b399f221774ad359e647bd2532cee668
Sara Benel <sbenelramirez@gmail.com>
2019-11-14 12:12:10 -0500

    algunas adaptaciones para la nueva plantilla

--- assets/js/user.js
+++ assets/js/user.js
@@ -1,12 +1,30 @@
 function validar_usuario() {
 	var user = $("#usuario").val();
 	$.ajax({
-		url: "lista.php",
+		url: "user_validation.php",
 		dataType: "text",
 		type: "post",
 		data: { user: user },
 		success: function(data) {
-			$("#scnd").html(data);
+			$("#pss").html(data);
+			$("#checkbox").html();
+		},
+		error: function(jqXhr, textStatus, errorThrown) {
+			console.log(errorThrown);
+		}
+	});
+	paginacion();
+}
+
+function login() {
+	var user = $("#usuario").val();
+	$.ajax({
+		url: "user_validation.php",
+		dataType: "text",
+		type: "post",
+		data: { user: user },
+		success: function(data) {
+			$("#pss").html(data);
 		},
 		error: function(jqXhr, textStatus, errorThrown) {
 			console.log(errorThrown);
--- php/user_validation.php
+++ php/user_validation.php
@@ -11,5 +11,13 @@ if($cantreg==1) {
   session_start();
   $reg = $rs->fetchObject();
   $_SESSION['usuario']=$reg->usuario;
-  header("location: access.php");
+  echo("<input type='password' id='pass' name='pass' class='form-control' placeholder='Password'>
+        <label class='checkbox'>
+          <span class='pull - right' > 
+            < a data - toggle='modal' href = 'login.html#myModal' > Forgot Password ?</a >
+          </span >
+        </label>
+        <button class='btn btn-theme btn-block' onclick='login()' type='submit'><i class='fa fa-lock'></i> SIGUIENTE</button>
+  ");
+
 } else header("location: login.php?mensaje=errorCredenciales");
