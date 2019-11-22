fd170cebc0cda3300b1caabb59866c5647484178
Sara Benel <sbenelramirez@gmail.com>
2019-11-22 01:00:35 -0500

    me falto este php, ahora si todo funciona como antes

--- php/buscar_repo.php
+++ php/buscar_repo.php
@@ -11,15 +11,15 @@ $caracteres_buenos = array("& lt;", "& gt;", "& quot;", "& #x27;", "& #x2F;", "&
 $consultaBusqueda = str_replace($caracteres_malos, $caracteres_buenos, $buscar);
 
 if (isset($consultaBusqueda)) {
-    $sql="SELECT * FROM repositorio inner join desarrollador on repositorio.idREPOSITORIO = desarrollador.idREPOSITORIO 
-    WHERE repositorio.nombre LIKE '%$consultaBusqueda%' and desarrollador.idUSUARIO = $user and desarrollador.idTIPODESARROLLADOR=1 ";
+    $sql="SELECT * FROM repositorio inner join desarrollador on repositorio.idrepositorio = desarrollador.idrepositorio
+    WHERE repositorio.nombre LIKE '%$consultaBusqueda%' and desarrollador.idusuario = $user and desarrollador.idtipodesarrollador=1 ";
     $result = $cnx->query($sql) or die($sql);
     while ($reg = $result->fetchObject() ){
         echo ("<div class='showback'>
                             <h4><i class='fa fa-angle-right'></i>$reg->nombre</h4>
                             <p>$reg->descripcion</p>                          
                             <button type='button' class='btn btn-default'>
-                                <a href='http://localhost/theme/html/myrepo.php?repo=$reg->idREPOSITORIO' >Entrar</a>
+                                <a href='http://localhost/theme/html/myrepo.php?repo=$reg->idrepositorio' >Entrar</a>
                             </button>
                         </div>");
     
